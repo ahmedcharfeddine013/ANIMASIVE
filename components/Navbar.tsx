9c1043de4b51558ea857cd41d443da2df7efc3de
John Doe <johndoe@email.com>
2024-01-18 12:35:12 +0100

    commit

---
+++ components/Navbar.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { ThemeToggle } from './theme-toggle';
+
+const Navbar = () => {
+  return (
+    <div>
+      <ThemeToggle />
+    </div>
+  )
+}
+
+export default Navbar
