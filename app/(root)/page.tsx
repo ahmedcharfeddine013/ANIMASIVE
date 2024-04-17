c08813a81c864154f6a75b821d30d90973b959db
ahmedcharfeddine013 <ahmedcharfeddine75@gmail.com>
2024-04-17 12:25:28 +0100

    prisma fixed

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,6 +2,8 @@ import Hero from "@/components/hero/Hero";
 import React from "react";
 import TopSales from "../../components/top sales/TopSales";
 
+
+
 const Home = () => {
   return (
     <div className="h-full flex flex-1 flex-col items-center justify-center gap-20">
